2c37e78cf4c813e1404af148dad55b46e74cf3c8
Kamillo Ferry <kafe@kafe.dev>
2023-02-03 17:22:57 +0100

    Incremental api (still not working)

--- incraf.py
+++ incraf.py
@@ -2,17 +2,17 @@ import ipafair
 
 from clingo.core import TruthValue
 from clingo.control import Control
-from clingo.symbol import Number, Function
+from clingo.symbol import Number, Function, Symbol
 from clingo.solving import Model, SolveResult
 
 from clingox.program import Program, ProgramObserver
 from clingox.backend import SymbolicBackend
 
-from typing import List, cast
+from typing import List, cast, overload
 
 from debug import solve_and_dump_model
 
-class incr_af_solver: #(ipafair.AFSolver):
+class incr_af_solver(ipafair.AFSolver):
     def __init__(self, sigma: str, af_file: str | None = None):
         """
         Initializes an `AFSolver` instance using the initial AF provided in `af_file`
@@ -22,11 +22,15 @@ class incr_af_solver: #(ipafair.AFSolver):
         """
         def load_dimacs(af_file: str):
             with open(af_file, "r", encoding="utf-8") as f:
+                line_no = 0
+
                 parameters = f.readline().strip().split(" ")
+                line_no += 1
                 while parameters[0] == "c":
                     parameters = f.readline().strip().split(" ")
+                    line_no += 1
                 if parameters[0] != "p" or parameters[1] != "af":
-                    raise Exception
+                    raise StopIteration
 
                 n = int(parameters[2])
                 with SymbolicBackend(self.ctl.backend()) as backend:
@@ -34,94 +38,145 @@ class incr_af_solver: #(ipafair.AFSolver):
                         backend.add_external(Function("arg", [Number(v)]), TruthValue(True))
 
                     for line in f:
+                        line_no += 1
                         arguments = line.strip().split(" ")
                         if arguments[0] == "c":
                             continue
                         if len(arguments) != 2:
-                            raise Exception
+                            raise SyntaxError("Malformed adjacency list in line " + str(line_no))
 
                         v, u = arguments
                         v, u = int(v), int(u)
 
                         self.add_attack(v, u)
 
-        def load_asp(ctl: Control, af_file: str):
+        def load_asp(af_file: str):
             with open(af_file, "r", encoding="utf-8") as f:
                 tmp = Control()
                 tmp.load(af_file)
                 tmp.ground()
                 tmp.ground([("instance", [])])
 
-                with SymbolicBackend(ctl.backend()) as backend:
-                    for v in tmp.symbolic_atoms.by_signature("arg", 1):
-                        backend.add_external(v.symbol, TruthValue(True))
+                for v in tmp.symbolic_atoms.by_signature("arg", 1):
+                    self.add_argument(v.symbol)
 
-                    for e in tmp.symbolic_atoms.by_signature("att", 2):
-                        backend.add_external(e.symbol, TruthValue(True))
-        
+                for e in tmp.symbolic_atoms.by_signature("att", 2):
+                    self.add_attack(attack=e.symbol)
+      
+        # self.ctl = Control(arguments=["--models=0", "-ebrave"])
         self.ctl = Control(arguments=["--models=0"])
         self.prg = Program()
         self.ctl.register_observer(ProgramObserver(self.prg))
 
+        if sigma == "naive":
+            # self.ctl.load("dung/naive.dl")
+            self.ctl.load("incr-dung/naive.lp")
+        elif sigma == "adm":
+            self.ctl.load("incr-dung/adm.lp")
+
         if af_file:
             try:
                 load_dimacs(af_file)
-            except:
-                # self.ctl = Control(arguments=["--models=0", "-ebrave"])
-                self.ctl = Control(arguments=["--models=0"])
-                self.prg = Program()
-                self.ctl.register_observer(ProgramObserver(self.prg))
-                load_asp(self.ctl, af_file)
+            except StopIteration:
+                load_asp(af_file)
 
-        self.ctl.add("output_filter", [], "#show in/1.")
-        
-        if sigma == "naive":
-            self.ctl.load("dung/naive.dl")
-            self.ctl.load("incr-dung/naive.lp")
-
+        # self.ctl.add("output_filter", [], "#show in/1.")
+    
         self.ctl.ground()
 
     def __del__(self):
         pass
 
-    def add_argument(self, arg: int):
+    def add_argument(self, arg: int | Symbol):
         '''
         Adds the argument `arg` to the current AF instance.
         '''
-        old = Function("arg", [Number(arg)]) in self.ctl.symbolic_atoms
+        if isinstance(arg,int):
+            old = Function("arg", [Number(arg)]) in self.ctl.symbolic_atoms
 
-        # self.del_vertex(v)
-        with SymbolicBackend(self.ctl.backend()) as backend:
-            backend.add_external(Function("arg", [Number(arg)]), TruthValue(True))
+            with SymbolicBackend(self.ctl.backend()) as backend:
+                backend.add_external(Function("arg", [Number(arg)]), TruthValue(True))
 
-        if not old: self.ctl.ground([("add_arg", [Number(arg)])])
+            if not old: self.ctl.ground([("add_argument", [Number(arg)])])
 
-    def del_argument(self, arg: int):
+        elif isinstance(arg,Symbol):
+            old = arg in self.ctl.symbolic_atoms
+            print(old, arg)
+
+            with SymbolicBackend(self.ctl.backend()) as backend:
+                backend.add_external(arg, TruthValue(True))
+
+            if not old: self.ctl.ground([("add_argument", arg.arguments)])
+
+        else:
+            raise TypeError("arg must be an argument id (int) or a clingo Symbol.")
+
+    def del_argument(self, arg: int | Symbol):
         '''
         Deletes the argument arg from the current AF instance.
         '''
-        self.ctl.assign_external(Function("arg", [Number(arg)]), False)
+        if isinstance(arg,int):
+            self.ctl.assign_external(Function("arg", [Number(arg)]), False)
 
-        for edge in self.ctl.symbolic_atoms.by_signature("att", 2):
-            A, B = edge.symbol.arguments
-            if Number(arg) == A or Number(arg) == B:
-                self.ctl.assign_external(Function("att", [A, B]), False)
+            for edge in self.ctl.symbolic_atoms.by_signature("att", 2):
+                A, B = edge.symbol.arguments
+                if Number(arg) == A or Number(arg) == B:
+                    self.ctl.assign_external(Function("att", [A, B]), False)
 
-    def add_attack(self, source: int, target: int):
-        old = Function("att", [Number(source), Number(target)]) in self.ctl.symbolic_atoms
+        elif isinstance(arg,Symbol):
+            self.ctl.assign_external(arg, False)
 
-        with SymbolicBackend(self.ctl.backend()) as backend:
-            backend.add_external(Function("att", [Number(source), Number(target)]), TruthValue(True))
+            for edge in self.ctl.symbolic_atoms.by_signature("att", 2):
+                A, B = edge.symbol.arguments
+                if arg.arguments[0] == A or arg.arguments[0] == B:
+                    self.ctl.assign_external(Function("att", [A, B]), False)
 
-        if not old: self.ctl.ground([("add_attack", [Number(source), Number(target)])])
+        else:
+            raise TypeError("arg must be an argument id (int) or a clingo Symbol.")
 
-    def del_attack(self, source: int, target: int):
-        self.ctl.assign_external(Function("att", [Number(source), Number(target)]), False)
+    def add_attack(self, source: int | None = None, target: int | None = None, attack: Symbol | None = None):
+        if isinstance(source, int) and isinstance(target, int) and attack == None:
+            old = Function("att", [Number(source), Number(target)]) in self.ctl.symbolic_atoms
+
+            with SymbolicBackend(self.ctl.backend()) as backend:
+                backend.add_external(Function("att", [Number(source), Number(target)]), TruthValue(True))
+
+            if not old: self.ctl.ground([("add_attack", [Number(source), Number(target)])])
+
+        elif isinstance(attack, Symbol) and source == None and target == None:
+            old = attack in self.ctl.symbolic_atoms
+            print(old, attack)
+
+            with SymbolicBackend(self.ctl.backend()) as backend:
+                backend.add_external(attack, TruthValue(True))
+
+            if not old: self.ctl.ground([("add_attack", attack.arguments)])
+
+        elif isinstance(attack, Symbol) and (source != None or target != None):
+            raise TypeError("attack and (source,target) parameters are mutually exclusive.")
+
+        else:
+            raise Exception
+
+    def del_attack(self, source: int | None = None, target: int | None = None, attack: Symbol | None = None):
+        if isinstance(source, int) and isinstance(target, int) and attack == None:
+            self.ctl.assign_external(Function("att", [Number(source), Number(target)]), False)
+
+        elif isinstance(attack, Symbol) and source == None and target == None:
+            self.ctl.assign_external(attack, False)
+
+        elif isinstance(attack, Symbol) and (source != None or target != None):
+            raise TypeError("attack and (source,target) parameters are mutually exclusive.")
+
+        else:
+            raise Exception
 
     def __solve(self, assumps: List[int] = []) -> bool:
         def on_model(m: Model):
-            print(m)
+            self.model_count += 1
+            print("Model:", m)
 
+        self.model_count = 0
         result = self.ctl.solve(assumps, on_model=on_model)
         return cast(SolveResult, result).satisfiable == True
 
@@ -154,18 +209,16 @@ class incr_af_solver: #(ipafair.AFSolver):
         '''
         raise NotImplementedError
 
-
-
 if __name__ == "__main__":
     def dump_ground_program(solver):
         print("\n === Ground program ===")
         print(solver.prg)
 
-    af = incr_af_solver("naive", "asp/af1.lp")
+    af = incr_af_solver("adm", "asp/af2.lp")
     dump_ground_program(af)
 
-    print("\n\n === Solve naively credulously ===")
-    af.solve_cred()
-    print("\n\n === Solve naively skeptically === ")
-    af.solve_skept()
+    print("\n\n === Solve credulously ===")
+    print(af.solve_cred(), af.model_count)
+    print("\n\n === Solve skeptically === ")
+    print(af.solve_skept(), af.model_count)
     # solve_and_dump_model(af.ctl)
